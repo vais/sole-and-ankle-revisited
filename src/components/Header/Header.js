24c9176cbd0a7957a36263dd7dbba38e1d03e6c2
Vais Salikhov <vsalikhov@gmail.com>
2022-03-11 22:09:52 -0500

    fluid desktop navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -65,8 +65,8 @@ const MainHeader = styled.div`
   display: flex;
   align-items: baseline;
   padding: 18px 32px;
-  height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  overflow-x: auto;
 
   @media ${QUERIES.tabletAndSmaller} {
     border-top: 4px solid ${COLORS.gray[900]};
@@ -76,7 +76,7 @@ const MainHeader = styled.div`
 
 const DesktopNav = styled.nav`
   display: flex;
-  gap: 48px;
+  gap: clamp(1rem, 8.1vw - 4rem, 3rem);
   margin: 0px 48px;
 
   @media ${QUERIES.tabletAndSmaller} {
